518bc5425cd79df13c42662f6a0aa5a5edf7ed23
DJ-kkk <129987327+DJ-kkk@users.noreply.github.com>
2024-07-25 19:24:11 +0100

    Add files via upload

---
+++ untitled8.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+root=Tk()
+root.title("Projwct 70")
+
+root.geometry("1600x400")
+root.configure( background ='lavender blush')
+
+
+
+
+def circle(event):
+    if keypress=='c'
+    draw_circle=canvas.create_oval(oldx,oldy,newx,newy, fill=color)
+    
+    
+    
+    
+root.bind("<c>",circle)
+    
+    
+
+draw(keypress,odlx,oldy,newy,newx)
+
+
+
+root.mainloop()
+
+
+        
